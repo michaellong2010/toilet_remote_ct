7778580273d720de8764fa176e442a05e547f884
michael <michael.long2010@gmail.com>
2015-10-25 10:43:25 +0800

    20151025 10:43 AM intelligent toilet remote control microchip project first commit

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=toilet_remote_ctl.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/toilet_remote_ctl.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=toiletremotectl.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/toiletremotectl.x.tar
