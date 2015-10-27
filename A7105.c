9c6e500283fc20687e379b2a3ec9635b00a2c466
michael <michael.long2010@gmail.com>
2015-10-27 08:54:56 +0800

    20151027 08:55 AM

---
+++ A7105.c
@@ -0,0 +1,21 @@
+#include "A7105reg.h"
+
+
+/*********************************************************************
+** initRF
+*********************************************************************/
+void initRF(void)
+{
+    //init io pin
+    /*SCS = 1;
+    SCK = 0;
+    SDIO = 1;
+    CKO = 1;
+    GIO1 = 1;
+    GIO2 = 1;
+
+    A7105_Reset(); //reset A7105 RF chip
+	A7105_WriteID(); //write ID code
+	A7105_Config(); //config A7105 chip
+	A7105_Cal(); //calibration IF,vco, */
+}
---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+ * File:   main.c
+ * Author: USER
+ *
+ * Created on 2015?10?27?, ?? 8:14
+ */
+
+
+#include <xc.h>
+#include "A7105reg.h"
+
+void main(void) {
+    initRF ();
+    return;
+}
